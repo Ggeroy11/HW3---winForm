3cf7035b066457508659f067a13651d761f9d032
Ggeroy11 <elnar1995@mail.ru>
2017-10-04 12:33:39 +0600

    Домашнее задание 3 MonthCalendar  выполнено

---
+++ DateTimeChecked/MonthCalendarForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DateTimeChecked
+{
+    partial class MyMonthCalendarForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(53, 18);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.MonthCalendar_DateChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите Дату";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 215);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // MyMonthCalendarForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "MyMonthCalendarForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ DateTimeChecked/MonthCalendarForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimeChecked
+{
+    public partial class MyMonthCalendarForm : Form
+    {
+        public MyMonthCalendarForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MonthCalendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            textBox1.Text = e.End.DayOfWeek.ToString();
+            switch (e.End.DayOfWeek) {
+                case DayOfWeek.Monday:
+                    textBox1.Text = "Понедельник";
+                    break;
+                case DayOfWeek.Tuesday:
+                    textBox1.Text = "Вторник";
+                    break;
+                case DayOfWeek.Wednesday:
+                    textBox1.Text = "Среда";
+                    break;
+                case DayOfWeek.Thursday:
+                    textBox1.Text = "Четверг";
+                    break;
+                case DayOfWeek.Friday:
+                    textBox1.Text = "Пятница";
+                    break;
+                case DayOfWeek.Saturday:
+                    textBox1.Text = "Суббота";
+                    break;
+                case DayOfWeek.Sunday:
+                    textBox1.Text = "Воскресенье";
+                    break;
+            }
+        }
+    }
+}
